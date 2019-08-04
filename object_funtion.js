b14fa3ea1eeeaa4a58961864884ffbf1148f9c8f
taeyeon <ki1556ki@naver.com>
2019-08-04 23:45:23 +0900

    d

---
+++ object_funtion.js
@@ -0,0 +1,10 @@
+var kim = {name:'kim', first:10, second:20}
+var lee = {name:'lee', first:10, second:10}
+function sum(prefix){
+    return prefix + (this.first + this.second);
+}
+// sum();
+console.log('sum.call(kim)', sum.call(kim, '=> '));
+console.log('lee.call(kim)', sum.call(lee, ': '));
+var kimSum = sum.bind(kim, '-> ');
+console.log('kimSum()', kimSum());
